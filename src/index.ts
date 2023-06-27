d916fcddec5e612eabc811b390cdbb8593f59751
dboland77 <dboland77@gmail.com>
2023-06-27 11:52:10 +0100

    added basic express server and dockerfiles

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hello world!');
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+const port = Number(process.env.PORT || 8080);
+
+// The host 0.0.0.0 is docker-specific localhost
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server listening on port ${port}`);
+});
